5b32d0b8bff270b36338947043ed43a9062e7f35
PrinceSingh2342 <PrinceSingh2342@gmail.com>
2024-03-03 10:41:05 +0530

    Changes in code

--- USStateXwalk.Designer.cs
+++ USStateXwalk.Designer.cs
@@ -28,20 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.ProcessXwalkCrawl = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StateCDcomboBox = new System.Windows.Forms.ComboBox();
+            this.DownloadXwalkPath = new System.Windows.Forms.TextBox();
+            this.btnbrowsefolder = new System.Windows.Forms.Button();
+            this.fdbbrowsefolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
-            // Form1
+            // ProcessXwalkCrawl
+            // 
+            this.ProcessXwalkCrawl.Location = new System.Drawing.Point(426, 402);
+            this.ProcessXwalkCrawl.Name = "ProcessXwalkCrawl";
+            this.ProcessXwalkCrawl.Size = new System.Drawing.Size(329, 57);
+            this.ProcessXwalkCrawl.TabIndex = 0;
+            this.ProcessXwalkCrawl.Text = "Process Xwalk Crawl";
+            this.ProcessXwalkCrawl.UseVisualStyleBackColor = true;
+            this.ProcessXwalkCrawl.Click += new System.EventHandler(this.ProcessXwalkCrawl_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Statename";
+            // 
+            // StateCDcomboBox
+            // 
+            this.StateCDcomboBox.FormattingEnabled = true;
+            this.StateCDcomboBox.Location = new System.Drawing.Point(982, 63);
+            this.StateCDcomboBox.Name = "StateCDcomboBox";
+            this.StateCDcomboBox.Size = new System.Drawing.Size(121, 28);
+            this.StateCDcomboBox.TabIndex = 2;
+            this.StateCDcomboBox.SelectedIndexChanged += new System.EventHandler(this.StateCDcomboBox_SelectedIndexChanged);
+            // 
+            // DownloadXwalkPath
+            // 
+            this.DownloadXwalkPath.Location = new System.Drawing.Point(348, 188);
+            this.DownloadXwalkPath.Name = "DownloadXwalkPath";
+            this.DownloadXwalkPath.Size = new System.Drawing.Size(475, 26);
+            this.DownloadXwalkPath.TabIndex = 3;
+            // 
+            // btnbrowsefolder
+            // 
+            this.btnbrowsefolder.Location = new System.Drawing.Point(901, 191);
+            this.btnbrowsefolder.Name = "btnbrowsefolder";
+            this.btnbrowsefolder.Size = new System.Drawing.Size(154, 27);
+            this.btnbrowsefolder.TabIndex = 4;
+            this.btnbrowsefolder.Text = "Browse Folder";
+            this.btnbrowsefolder.UseVisualStyleBackColor = true;
+            // 
+            // USStateXwalk
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1084, 538);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1249, 641);
+            this.Controls.Add(this.btnbrowsefolder);
+            this.Controls.Add(this.DownloadXwalkPath);
+            this.Controls.Add(this.StateCDcomboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProcessXwalkCrawl);
+            this.Name = "USStateXwalk";
             this.Text = "USStateXwalk";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ProcessXwalkCrawl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox StateCDcomboBox;
+        private System.Windows.Forms.TextBox DownloadXwalkPath;
+        private System.Windows.Forms.Button btnbrowsefolder;
+        private System.Windows.Forms.FolderBrowserDialog fdbbrowsefolder;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- USStateXwalk.cs
+++ USStateXwalk.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,28 @@ namespace USStateXwalk
 {
     public partial class USStateXwalk : Form
     {
+        string lstr;
+        HttpWebRequest request;
+        HttpWebResponse response = null/* TODO Change to default(_) if this is not a reference type */;
+        StreamReader reader;
+        string strSiteUrlFromAppConfig = ConfigurationManager.AppSettings["SiteUrlFromAppConfig"];
         public USStateXwalk()
         {
             InitializeComponent();
+        }
+
+        private void ProcessXwalkCrawl_Click(object sender, EventArgs e)
+        {
+            request = (HttpWebRequest)WebRequest.Create(strSiteUrlFromAppConfig);
+            response = (HttpWebResponse)request.GetResponse();
+            reader = new StreamReader(response.GetResponseStream());
+            lstr = reader.ReadToEnd();
+        }
+
+        private void StateCDcomboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string StateCDName = ConfigurationManager.AppSettings["StateName"];
+
         }
     }
 }
